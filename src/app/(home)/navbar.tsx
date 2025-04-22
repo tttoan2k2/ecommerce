da5fa31ceb2743a40aec437e3bd314408db11b1f
tttoan2k2 <toantran@toanit.localdomain>
2025-04-23 00:09:09 +0700

    03: home layout fix

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
                     variant="secondary"
                     className="bg-white border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none hover:bg-pink-400 transition-colors text-lg"
                 >
-                    <Link href="sign-in">Log in</Link>
+                    <Link href="/sign-in">Log in</Link>
                 </Button>
 
                 <Button
@@ -90,7 +90,7 @@ const Navbar = () => {
                     variant="secondary"
                     className="bg-black text-white border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none hover:bg-pink-400 hover:text-black transition-colors text-lg"
                 >
-                    <Link href="sign-up">Start selling</Link>
+                    <Link href="/sign-up">Start selling</Link>
                 </Button>
             </div>
 
