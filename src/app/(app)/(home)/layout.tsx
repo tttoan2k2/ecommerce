0d41f74b2450b6fa0e104818cba972d8fc8f73a1
tttoan2k2 <toantran@toanit.localdomain>
2025-04-23 22:44:02 +0700

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,45 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
 import Footer from "./footer";
 import Navbar from "./navbar";
+import { SearchFilters } from "./search-filters";
+import { Category } from "@/payload-types";
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+    const payload = await getPayload({
+        config: configPromise,
+    });
+
+    const data = await payload.find({
+        collection: "categories",
+        depth: 1,
+        pagination: false,
+        where: {
+            parent: {
+                exists: false,
+            },
+        },
+    });
+
+    const fomattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category),
+            subcategories: undefined,
+        })),
+    }));
+
+    console.log(data, fomattedData);
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={fomattedData} />
             <div className="flex-1 bg-[#F4F4F0]">{children}</div>
             <Footer />
         </div>
