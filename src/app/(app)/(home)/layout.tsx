88c9b6b857ab7c8f3366567ede38d61e9efe92a7
tttoan2k2 <toantran@toanit>
2025-04-24 19:08:47 +0700

    06: categories finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./footer";
 import Navbar from "./navbar";
 import { SearchFilters } from "./search-filters";
 import { Category } from "@/payload-types";
+import { CustomCategory } from "./types";
 
 interface Props {
     children: React.ReactNode;
@@ -24,9 +25,10 @@ const Layout = async ({ children }: Props) => {
                 exists: false,
             },
         },
+        sort: "name",
     });
 
-    const formattedData = data.docs.map((doc) => ({
+    const formattedData: CustomCategory[] = data.docs.map((doc) => ({
         ...doc,
         subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             ...(doc as Category),
--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,13 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+
+import { CustomCategory } from "../types";
+
+import { CategoriesSidebar } from "./categories-sidebar";
 
 interface Props {
     disabled?: boolean;
+    data: CustomCategory[];
 }
 
-export const SearchInput = ({ disabled }: Props) => {
+export const SearchInput = ({ disabled, data }: Props) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
         <div className="flex items-center gap-2 w-full">
+            <CategoriesSidebar
+                data={data}
+                open={isSidebarOpen}
+                onOpenChange={setIsSidebarOpen}
+            />
             <div className="relative w-full">
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
                 <Input
@@ -16,6 +34,13 @@ export const SearchInput = ({ disabled }: Props) => {
                     disabled={disabled}
                 />
             </div>
+            <Button
+                variant="elevated"
+                className="size-12 shrink-0 flex lg:hidden"
+                onClick={() => setIsSidebarOpen(true)}
+            >
+                <ListFilterIcon className="size-4" />
+            </Button>
         </div>
     );
 };
