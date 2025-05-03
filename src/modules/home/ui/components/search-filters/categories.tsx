a0fdd66fb899be16a980cc0f0ac0355a2debf41f
tttoan2k2 <toantran@toanit>
2025-05-03 13:37:45 +0700

    10: category pages

--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -10,12 +10,15 @@ import { ListFilterIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { CategoriesSidebar } from "./categories-sidebar";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
+import { useParams } from "next/navigation";
 
 interface CategoriesProps {
     data: CategoriesGetManyOutput;
 }
 
 export const Categories = ({ data }: CategoriesProps) => {
+    const params = useParams();
+
     const containerRef = useRef<HTMLDivElement>(null);
     const measureRef = useRef<HTMLDivElement>(null);
     const viewAllRef = useRef<HTMLDivElement>(null);
@@ -24,7 +27,9 @@ export const Categories = ({ data }: CategoriesProps) => {
     const [isAnyHovered, setIsAnyHovered] = useState(false);
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const activeCategory = "all";
+    const categoryParam = params.category as string | undefined;
+
+    const activeCategory = categoryParam || "all";
 
     const activeCategoryIndex = data.findIndex(
         (cat) => cat.slug === activeCategory
@@ -112,6 +117,7 @@ export const Categories = ({ data }: CategoriesProps) => {
 
                 <div ref={viewAllRef} className="shrink-0">
                     <Button
+                        variant="elevated"
                         className={cn(
                             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                             isActiveCategoryHidden &&
